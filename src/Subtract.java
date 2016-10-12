d18c3d6d601431735cf9ac2947e47dff1ded5872
nishakar123 <nishakar123@gmail.com>
2016-10-12 13:33:39 +0530

    Initial Commit.

---
+++ src/Subtract.java
@@ -0,0 +1,15 @@
+
+public class Subtract {
+
+	public static void main(String[] args) {
+		int sum=100;
+		for (int i = 0; i < 100; i++) {
+			sum-=1;
+			if(sum==50)
+				break;
+		}
+		System.out.println(sum);
+
+	}
+
+}
